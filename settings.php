0a3af47368b40caf6df1288db47e2d7c69a060ee
Jose Maria Garcia <jmgarciacarrasco@gmail.com>
2017-12-28 09:33:11 +0100

    ok

--- settings.php
+++ settings.php
@@ -357,6 +357,7 @@
                          <div style="margin-top:20px;">
                              <div>
                                  <!-- Language -->
+                                 <? $_SERVER['HTTP_CLIENT_IP'] ?>
                                  <div class="title">
                                      <img src="Images/Button-empathy-icon.png" style="vertical-align:middle; padding-right:10px;">
                                      <strong>Localización</strong>
